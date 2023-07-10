8b4f07916af8e12f44c00c88185c0fe559ee4dd4
n.behnam <n.behnam@abacogroup.eu>
2023-07-10 12:48:57 +0200

    End-Video-132

---
+++ src/components/TheHero.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="flex h-screen flex-col bg-yellow-200 pb-20 pt-10">
+    This is the hero section
+  </main>
+</template>
+
+<script>
+export default {
+  name: "TheHero",
+};
+</script>
+
+<style></style>
