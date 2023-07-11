421d7bb1a092a6ca3b911ab6867a3927ee77d6f1
n.behnam <n.behnam@abacogroup.eu>
2023-07-11 10:30:21 +0200

    Video-134 Grid System

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,6 +1,11 @@
 <template>
   <main class="flex h-screen flex-col bg-yellow-200 pb-20 pt-10">
-    This is the hero section
+    <div class="grid grid-cols-12">
+      <div class="col-span-1 col-start-1 border border-blue-700">col-1</div>
+      <div class="col-span-5 col-start-2 border border-blue-700">col-2</div>
+      <div class="col-span-5 col-start-7 border border-blue-700">col-3</div>
+      <div class="col-span-1 col-start-12 border border-blue-700">col-4</div>
+    </div>
   </main>
 </template>
 
